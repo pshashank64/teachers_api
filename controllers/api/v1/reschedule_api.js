fe349be5ccdcad57482dc8e454ed14c2fa7d927f
Shashank <pshashank64@gmail.com>
2022-02-07 21:29:27 +0530

    final changes are done and api is sending data through frontend as well as through postman!

--- controllers/api/v1/reschedule_api.js
+++ controllers/api/v1/reschedule_api.js
@@ -2,7 +2,24 @@ const Batch = require("../../../models/batches");
 
 module.exports.reschedule = async (req, res) => {
     let find_batch = await Batch.findOne({courseId: req.body.courseId})
+
+    Batch.findOneAndUpdate(find_batch, {
+        date: req.body.date,
+        time: req.body.time
+    }).then(() => {
+        Batch.findOne({courseId: req.body.courseId}).then((item) => {
+            res.send({
+                "data": item,
+                "satatus": 200,
+                "message": "timetable rescheduled!"
+            });
+            console.log(item);
+        });
+    }).catch(err => {
+        res.status(400).send("unable to reschedule the batch!");
+    });
     
+
     // let find_batch = await Batch.findOne({courseId: req.body.courseId})
     // find_batch.updateOne({
     //     date: req.body.date,
--- routes/api/v1/batch_reschedule.js
+++ routes/api/v1/batch_reschedule.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const rescheduleApi = require("../../../controllers/api/v1/reschedule_api");
 
-router.put("/reschedule-batch", rescheduleApi.reschedule);
+router.post("/reschedule-batch", rescheduleApi.reschedule);
 
 module.exports = router;
